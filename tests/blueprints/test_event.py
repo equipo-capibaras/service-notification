9d6d6de1cd9afebdf8c35147a4df7538d288a25f
Tim Pambor <t.pambor@uniandes.edu.co>
2024-11-10 20:14:25 +0100

    PF-327: Implement unit tests

--- tests/blueprints/test_event.py
+++ tests/blueprints/test_event.py
@@ -1,26 +1,26 @@
 from typing import Any, cast
-from unittest import TestCase
 from unittest.mock import Mock
 
 from faker import Faker
+from unittest_parametrize import ParametrizedTestCase, parametrize
 
 from app import create_app
 from models import Action, Channel, Plan, Role
 from repositories import MailRepository
 
 
-class TestEvent(TestCase):
+class TestEvent(ParametrizedTestCase):
     def setUp(self) -> None:
         self.faker = Faker()
 
         self.app = create_app()
         self.client = self.app.test_client()
 
-    def gen_random_event_data(self) -> dict[str, Any]:
+    def gen_random_event_data(self, channel: Channel | None = None) -> dict[str, Any]:
         return {
             'id': cast(str, self.faker.uuid4()),
             'name': self.faker.sentence(3),
-            'channel': self.faker.random_element(list(Channel)),
+            'channel': channel or self.faker.random_element(list(Channel)),
             'language': self.faker.random_element(['es', 'pt']),
             'reportedBy': {
                 'id': cast(str, self.faker.uuid4()),
@@ -47,12 +47,6 @@ class TestEvent(TestCase):
                     'action': Action.CREATED,
                     'description': self.faker.text(200),
                 },
-                {
-                    'seq': 1,
-                    'date': self.faker.past_datetime().isoformat().replace('+00:00', 'Z'),
-                    'action': Action.ESCALATED,
-                    'description': self.faker.text(200),
-                },
             ],
             'client': {
                 'id': cast(str, self.faker.uuid4()),
@@ -62,10 +56,36 @@ class TestEvent(TestCase):
             },
         }
 
-    def test_health(self) -> None:
+    @parametrize(
+        ('state', 'channel', 'expect_mail'),
+        [
+            ('created', Channel.EMAIL, False),
+            ('created', Channel.WEB, True),
+            ('created', Channel.MOBILE, True),
+            ('escalated', None, True),
+            ('closed', None, True),
+        ],
+    )
+    def test_update(self, *, state: str, channel: Channel | None, expect_mail: bool) -> None:
         mail_repo_mock = Mock(MailRepository)
 
+        data = self.gen_random_event_data(channel=channel)
+        if state != 'created':
+            data['history'].append(
+                {
+                    'seq': 1,
+                    'date': self.faker.past_datetime().isoformat().replace('+00:00', 'Z'),
+                    'action': Action.ESCALATED if state == 'escalated' else Action.CLOSED,
+                    'description': self.faker.text(200),
+                },
+            )
+
         with self.app.container.mail_repo.override(mail_repo_mock):
-            resp = self.client.post('/api/v1/incident-update/notification', json=self.gen_random_event_data())
+            resp = self.client.post('/api/v1/incident-update/notification', json=data)
+
+        if expect_mail:
+            cast(Mock, mail_repo_mock.send).assert_called_once()
+        else:
+            cast(Mock, mail_repo_mock.send).assert_not_called()
 
         self.assertEqual(resp.status_code, 200)
